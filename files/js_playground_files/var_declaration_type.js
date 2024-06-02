20f312b218e8bd8ddfa5d6f7343f9e32c0f63d55
Anurudha_Singh <singhanurudh570@gmail.com>
2024-06-02 15:54:02 +0530

    Implementted var, let, and const keywords

---
+++ files/js_playground_files/var_declaration_type.js
@@ -0,0 +1,5 @@
+var a="I am declared using var keyword."
+let b="I am declared using let keyword."
+const c="I am declared using const keyword."
+
+console.log(`Value of a is:: ${a}, b is:: ${b}, and c is ${c}`)
